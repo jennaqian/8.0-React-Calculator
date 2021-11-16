eb6d205bccb492c9f4d18e09ccf1ad18817f4330
Jenna Qian <jiayin1996@gmail.com>
2021-11-15 23:10:13 -0500

    Add Buttons Component

---
+++ 8.2-react-calculator/src/App.js
@@ -0,0 +1,15 @@
+import {Component} from "react"
+import Buttons from "./components/Buttons.js"
+
+class App extends Component {
+
+  render () {
+    return (
+      <div>
+        <Buttons />
+      </div>
+    )
+  }
+}
+
+export default App;
