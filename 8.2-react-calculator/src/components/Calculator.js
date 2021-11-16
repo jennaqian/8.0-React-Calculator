9d1bdd40d8db21f7c170c625aa19f5150ca4b40a
Jenna Qian <jiayin1996@gmail.com>
2021-11-16 12:02:12 -0500

    Handle Operations

--- 8.2-react-calculator/src/components/Calculator.js
+++ 8.2-react-calculator/src/components/Calculator.js
@@ -1,3 +1,4 @@
+import { id } from "prelude-ls";
 import { Component } from "react";
 
 class Calculator extends Component {
@@ -7,18 +8,50 @@ class Calculator extends Component {
     this.state = {
       input: 0,
       operation: null,
-      result: null,
+      num: 0,
+      storedNum: 0,
     };
   }
 
   handleInputChange = (e) => {
-    console.log(e.target.value)
+    this.setState({
+      input: e.target.value,
+      num: e.target.value
+    })
   }
 
   handleOperation = (e) => {
     this.setState({
-        operation: e.target.value
+        operation: e.target.value,
+        storedNum: this.state.input 
     })
+  }
+
+  handleResult = () => {
+    const {operation, num, storedNum} = this.state
+    console.log(operation)
+    switch (operation) {
+      case "+":
+        return this.setState({
+          input: Number(num) + Number(storedNum)
+        });
+
+      case "-":
+        return this.setState({
+          input: Number(storedNum) - Number(num)
+        });
+      
+      case "/":
+        return this.setState({
+          input: (Number(storedNum) / Number(num))
+        });
+
+      case "x":
+        return this.setState({
+          input: Number(storedNum) * Number(num)
+        });
+    }
+
   }
 
   handleClear = () => {
@@ -33,23 +66,23 @@ class Calculator extends Component {
     const { input, result } = this.state;
     return (
       <div>
-        <input type="text" value={input} onChange={this.handleInputChange}/>
+        <input type="text" className="" value={input} onChange={this.handleInputChange}/>
 
-        <button value="9">9</button>
-        <button value="8">8</button>
-        <button value="7">7</button>
-        <button value="6">6</button>
-        <button value="5">5</button>
-        <button value="4">4</button>
-        <button value="3">3</button>
-        <button value="2">2</button>
-        <button value="1">1</button>
-        <button value="0">0</button>
+        <button value="9" onClick={this.handleInputChange}>9</button>
+        <button value="8" onClick={this.handleInputChange}>8</button>
+        <button value="7" onClick={this.handleInputChange}>7</button>
+        <button value="6" onClick={this.handleInputChange}>6</button>
+        <button value="5" onClick={this.handleInputChange}>5</button>
+        <button value="4" onClick={this.handleInputChange}>4</button>
+        <button value="3" onClick={this.handleInputChange}>3</button>
+        <button value="2" onClick={this.handleInputChange}>2</button>
+        <button value="1" onClick={this.handleInputChange}>1</button>
+        <button value="0" onClick={this.handleInputChange}>0</button>
         <button value="/" onClick={this.handleOperation}>&#247;</button>
         <button value="x" onClick={this.handleOperation}>&times;</button>
         <button value="+" onClick={this.handleOperation}>&#43;</button>
         <button value="-" onClick={this.handleOperation}>&#45;</button>
-        <button value="=">&#61;</button>
+        <button value="=" onClick={this.handleResult}>&#61;</button>
         <button value="AC" onClick={this.handleClear}>AC</button>
         <button value="%">%</button>
         <button value="+/-">+/-</button>
