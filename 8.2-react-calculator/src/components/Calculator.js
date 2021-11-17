647a7e0fb6f7f556e12eeff96700c18edc8b0b05
Jenna Qian <jiayin1996@gmail.com>
2021-11-17 09:53:46 -0500

    Added more functionality

--- 8.2-react-calculator/src/components/Calculator.js
+++ 8.2-react-calculator/src/components/Calculator.js
@@ -1,4 +1,3 @@
-import { id } from "prelude-ls";
 import { Component } from "react";
 
 class Calculator extends Component {
@@ -9,68 +8,86 @@ class Calculator extends Component {
       input: 0,
       operation: null,
       num: 0,
-      storedNum: 0,
     };
   }
 
   handleInputChange = (e) => {
     this.setState({
-      input: e.target.value,
-      num: e.target.value
+      input: this.state.input + e.target.value,
     })
   }
 
   handleOperation = (e) => {
     this.setState({
         operation: e.target.value,
-        storedNum: this.state.input 
+        num: Number(this.state.input),
+        input: "",
     })
   }
 
   handleResult = () => {
-    const {operation, num, storedNum} = this.state
-    console.log(operation)
+    const {operation, num, input} = this.state
     switch (operation) {
       case "+":
         return this.setState({
-          input: Number(num) + Number(storedNum)
+          input: num + Number(input)
         });
 
       case "-":
         return this.setState({
-          input: Number(storedNum) - Number(num)
+          input: num - Number(input)
         });
       
       case "/":
         return this.setState({
-          input: (Number(storedNum) / Number(num))
+          input: Math.round(num / Number(input))
         });
 
       case "x":
         return this.setState({
-          input: Number(storedNum) * Number(num)
+          input: num * Number(input)
         });
+
+      default: 
+        break;
     }
 
   }
+
+  handlePosOrNeg = (e) => {
+    if(e.target.value === "+/-") {
+      this.setState({
+        input: -this.state.input
+      })
+    } else if(e.target.value === "%") {
+      this.setState({
+        input: Number(this.state.input) / 100
+      })
+    }
+  }
+
 
   handleClear = () => {
       this.setState({
           input: 0,
           operation: null,
-          result: null,
+          num: null,
       })
+  }
+  
+  handleInput = (num) => {
+    return new Intl.NumberFormat().format(num)
   }
 
   render() {
-    const { input, result } = this.state;
+    const { input } = this.state;
     return (
       <div className="Calculator">
-        <input type="text" className="Display" value={input} onChange={this.handleInputChange}/>
+        <input type="text" className="Display" value={input[0] === "0" ? this.handleInput(Number(input.slice(1))): input} onChange={this.handleInputChange}/>
 
         <div className="Buttons">
           <div className="Numbers-Container">
-            <button value="9" className="Numbers" onClick={this.handleInputChange}>9</button>
+            <button value="9" className="Numbers" onClick={this.handleInputChange}>{"9"}</button>
             <button value="8" className="Numbers" onClick={this.handleInputChange}>8</button>
             <button value="7" className="Numbers" onClick={this.handleInputChange}>7</button>
             <button value="6" className="Numbers" onClick={this.handleInputChange}>6</button>
@@ -79,9 +96,9 @@ class Calculator extends Component {
             <button value="3" className="Numbers" onClick={this.handleInputChange}>3</button>
             <button value="2" className="Numbers" onClick={this.handleInputChange}>2</button>
             <button value="1" className="Numbers" onClick={this.handleInputChange}>1</button>
-            <button value=".">.</button>
+            <button value="." className="BtmBtns" onClick={this.handleInputChange}>.</button>
             <button value="0" className="Numbers" onClick={this.handleInputChange}>0</button>
-            <button value="="  onClick={this.handleResult}>&#61;</button>
+            <button value="=" className="BtmBtns" onClick={this.handleResult}>&#61;</button>
           </div>
           
           <div className="Operations-Container">
@@ -93,8 +110,8 @@ class Calculator extends Component {
 
           <div className="TopBtns-Container">
             <button value="AC" className="TopBtns" onClick={this.handleClear}>AC</button>
-            <button value="+/-" className="TopBtns">+/-</button>
-            <button value="%" className="TopBtns">%</button>
+            <button value="+/-" className="TopBtns" onClick={this.handlePosOrNeg}>+/-</button>
+            <button value="%" className="TopBtns" onClick={this.handlePosOrNeg}>%</button>
           </div>
           
         </div>
